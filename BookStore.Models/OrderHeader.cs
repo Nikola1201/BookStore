80ac222ea194cbbc8d1e5c6585073de042b41ea2
Nikola1201 <nikola.icic12@gmail.com>
2024-03-19 23:41:29 +0100

    Added Summary View and Summary ActionMethod in Controller Cart. Also created Model OrderHeader

---
+++ BookStore.Models/OrderHeader.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    public class OrderHeader
+    {
+        public int Id { get; set; }
+        public string ApplicationUserId { get; set; }
+        [ForeignKey("ApplicationUserId")]
+        [ValidateNever]
+        public ApplicationUser ApplicationUser { get; set; }
+        [Required]
+        public DateTime OrderDate { get; set; }
+        [Required]
+        public DateTime ShippingDate { get; set; }
+    }
+}
