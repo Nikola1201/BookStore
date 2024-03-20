ba031010c628a219effb1e55e15dcc7484eeb1ca
Nikola1201 <nikola.icic12@gmail.com>
2024-03-20 01:58:34 +0100

    Added StripeSettings with Secondary and Publishable key in appsetings.json and used it to connect to get ApiKey in order to add StripeConfiguration in the pipeline

--- BookStoreWeb/Program.cs
+++ BookStoreWeb/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using BookStore.Utility;
+using Stripe;
 
 namespace BookStoreWeb
 {
@@ -19,8 +20,10 @@ namespace BookStoreWeb
             builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
                 builder.Configuration.GetConnectionString("DefaultConnection")
                 ));
+            builder.Services.Configure<StripeSettings>(
+                builder.Configuration.GetSection("StripeSettings"));
 
-            builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddDefaultTokenProviders()
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultTokenProviders()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
             builder.Services.AddSingleton<IEmailSender, EmailSender>();
@@ -47,6 +50,8 @@ namespace BookStoreWeb
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            StripeConfiguration.ApiKey = builder.Configuration.GetSection("StripeSettings:SecretKey").Get<string>();
 
             app.UseAuthentication();
 
